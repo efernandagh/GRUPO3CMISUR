254d9f6334bfa4399a68d6a2c9a6d19b05f598a6
JosetpRam <clarosaron8@gmail.com>
2025-09-30 11:57:56 -0600

    Josetp

--- servicios.Designer.cs
+++ servicios.Designer.cs
@@ -53,94 +53,103 @@
             groupBox1.Controls.Add(label4);
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(label2);
-            groupBox1.Location = new Point(135, 36);
+            groupBox1.Location = new Point(154, 48);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(519, 335);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(593, 447);
             groupBox1.TabIndex = 23;
             groupBox1.TabStop = false;
             groupBox1.Text = "Seccion de servicios";
             // 
             // button3
             // 
-            button3.Location = new Point(331, 293);
+            button3.Location = new Point(378, 391);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(75, 23);
+            button3.Size = new Size(86, 31);
             button3.TabIndex = 32;
             button3.Text = "Editar";
             button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(219, 293);
+            button2.Location = new Point(250, 391);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(86, 31);
             button2.TabIndex = 31;
             button2.Text = "Eliminar";
             button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            button1.Location = new Point(112, 293);
+            button1.Location = new Point(128, 391);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 30;
             button1.Text = "Guardar";
             button1.UseVisualStyleBackColor = true;
             // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(163, 205);
+            numericUpDown1.Location = new Point(186, 273);
+            numericUpDown1.Margin = new Padding(3, 4, 3, 4);
             numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.Size = new Size(137, 27);
             numericUpDown1.TabIndex = 29;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(165, 147);
+            textBox2.Location = new Point(189, 196);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(118, 23);
+            textBox2.Size = new Size(134, 27);
             textBox2.TabIndex = 28;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(165, 91);
+            textBox1.Location = new Point(202, 118);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(118, 23);
+            textBox1.Size = new Size(134, 27);
             textBox1.TabIndex = 27;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(46, 205);
+            label4.Location = new Point(53, 273);
             label4.Name = "label4";
-            label4.Size = new Size(40, 15);
+            label4.Size = new Size(50, 20);
             label4.TabIndex = 26;
             label4.Text = "Precio";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(46, 147);
+            label3.Location = new Point(53, 196);
             label3.Name = "label3";
-            label3.Size = new Size(69, 15);
+            label3.Size = new Size(87, 20);
             label3.TabIndex = 25;
             label3.Text = "Descripcion";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(46, 94);
+            label2.Location = new Point(53, 125);
             label2.Name = "label2";
-            label2.Size = new Size(113, 15);
+            label2.Size = new Size(143, 20);
             label2.TabIndex = 24;
             label2.Text = "Nombre del servicio";
             // 
             // servicios
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(groupBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "servicios";
             Text = "servicios";
             Load += servicios_Load;
