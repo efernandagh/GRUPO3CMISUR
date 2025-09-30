4b29e6201a931ec7e90d494f05c626b00df7e57a
Alejandro Callison <alecmendieta11@gmail.com>
2025-09-30 08:32:48 -0600

    prueba

--- seguimiento.Designer.cs
+++ seguimiento.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            dataGridView1 = new DataGridView();
             button1 = new Button();
             comboBox2 = new ComboBox();
             textBox1 = new TextBox();
@@ -39,12 +38,14 @@
             label3 = new Label();
             label2 = new Label();
             label1 = new Label();
+            dataGridView1 = new DataGridView();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
             // 
+            groupBox1.BackColor = Color.SteelBlue;
             groupBox1.Controls.Add(button1);
             groupBox1.Controls.Add(comboBox2);
             groupBox1.Controls.Add(textBox1);
@@ -54,26 +55,21 @@
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(205, 12);
+            groupBox1.Location = new Point(234, 16);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(407, 284);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(465, 379);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Seccion de seguimiento";
             // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(249, 302);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(310, 108);
-            dataGridView1.TabIndex = 19;
-            // 
             // button1
             // 
-            button1.Location = new Point(169, 230);
+            button1.Location = new Point(193, 307);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 18;
             button1.Text = "Guardar";
             button1.UseVisualStyleBackColor = true;
@@ -81,76 +77,91 @@
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(150, 176);
+            comboBox2.Location = new Point(171, 235);
+            comboBox2.Margin = new Padding(3, 4, 3, 4);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(164, 23);
+            comboBox2.Size = new Size(187, 28);
             comboBox2.TabIndex = 17;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(150, 142);
+            textBox1.Location = new Point(171, 189);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(173, 23);
+            textBox1.Size = new Size(197, 27);
             textBox1.TabIndex = 16;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(150, 109);
+            dateTimePicker1.Location = new Point(171, 145);
+            dateTimePicker1.Margin = new Padding(3, 4, 3, 4);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.Size = new Size(228, 27);
             dateTimePicker1.TabIndex = 15;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(150, 79);
+            comboBox1.Location = new Point(171, 105);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(164, 23);
+            comboBox1.Size = new Size(187, 28);
             comboBox1.TabIndex = 14;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(20, 179);
+            label4.Location = new Point(23, 239);
             label4.Name = "label4";
-            label4.Size = new Size(101, 15);
+            label4.Size = new Size(128, 20);
             label4.TabIndex = 13;
             label4.Text = "Nivel satisfactorio";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(20, 142);
+            label3.Location = new Point(23, 189);
             label3.Name = "label3";
-            label3.Size = new Size(69, 15);
+            label3.Size = new Size(87, 20);
             label3.TabIndex = 12;
             label3.Text = "Descripcion";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(20, 115);
+            label2.Location = new Point(23, 153);
             label2.Name = "label2";
-            label2.Size = new Size(121, 15);
+            label2.Size = new Size(152, 20);
             label2.TabIndex = 11;
             label2.Text = "fecha de seguimiento";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(20, 82);
+            label1.Location = new Point(23, 109);
             label1.Name = "label1";
-            label1.Size = new Size(54, 15);
+            label1.Size = new Size(67, 20);
             label1.TabIndex = 10;
             label1.Text = "Contrato";
             // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(285, 403);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(354, 144);
+            dataGridView1.TabIndex = 19;
+            // 
             // seguimiento
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(dataGridView1);
             Controls.Add(groupBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "seguimiento";
             Text = "seguimiento";
             groupBox1.ResumeLayout(false);
